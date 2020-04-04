f8b6908e4fe6729cbb6f4459369b156be855d348
Wyndfall <aldenmauro@gmail.com>
2020-04-04 13:29:56 -0700

    Updated with combined files

--- Arduino/displayCode.ino
+++ Arduino/displayCode.ino
@@ -2,6 +2,8 @@
 #include <HyperDisplay_SSD1309.h>
 #include <hyperdisplay.h>
 
+#include <ArduinoBLE.h>
+
 #define SERIAL_PORT Serial  
 #define WIRE_PORT Wire      // Used if USE_SPI == 0
 #define SPI_PORT SPI        // Used if USE_SPI == 1
@@ -25,25 +27,91 @@
 char input[50] = "Hello world!";
 
 
+BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE LED Service
+
+// BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+BLEByteCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+
+const int ledPin = LED_BUILTIN; // pin to use for the LED
+
+
 void setup() {
   Serial.begin(9600);
 
-#if USE_SPI 
-  SPI_PORT.begin();
-  myTOLED.begin(CS_PIN, DC_PIN, SPI_PORT);                  // Begin for SPI requires that you provide the CS and DC pin numbers
-#else
-  WIRE_PORT.begin();
-  myTOLED.begin(WIRE_PORT, false, SSD1309_ARD_UNUSED_PIN);  // Begin for I2C has default values for every argument
-  Wire.setClock(400000);
-#endif /* USSE_SPI */
+  while (!Serial1);
+  
+    // set LED pin to output mode
+    pinMode(ledPin, OUTPUT);
+  
+    // begin initialization
+    if (!BLE.begin()) {
+      Serial1.println("starting BLE failed!");
+  
+      while (1);
+    }
+  #if USE_SPI 
+    SPI_PORT.begin();
+    myTOLED.begin(CS_PIN, DC_PIN, SPI_PORT);                  // Begin for SPI requires that you provide the CS and DC pin numbers
+  #else
+    WIRE_PORT.begin();
+    myTOLED.begin(WIRE_PORT, false, SSD1309_ARD_UNUSED_PIN);  // Begin for I2C has default values for every argument
+    Wire.setClock(400000);
+  #endif /* USSE_SPI */
+
+   // set advertised local name and service UUID:
+  BLE.setLocalName("LED");
+  BLE.setAdvertisedService(ledService);
+
+  // add the characteristic to the service
+  ledService.addCharacteristic(switchCharacteristic);
+
+  // add service
+  BLE.addService(ledService);
+
+  // set the initial value for the characeristic:
+  switchCharacteristic.writeValue(0);
+
+  // start advertising
+  BLE.advertise();
+
+  Serial1.println("BLE LED Peripheral");
 }
 
 void loop() {
+  BLEDevice central = BLE.central();
+
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial1.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial1.println(central.address());
+
+    // while the central is still connected to peripheral:
+    while (central.connected()) {
+      // if the remote device wrote to the characteristic,
+      // use the value to control the LED:
+      if (switchCharacteristic.written()) {
+        if (switchCharacteristic.value()) {   // any value other than 0
+          Serial1.println("LED on");
+          digitalWrite(ledPin, HIGH);         // will turn the LED on
+        } else {                              // a 0 value
+          Serial1.println(F("LED off"));
+          digitalWrite(ledPin, LOW);          // will turn the LED off
+        }
+      }
+    }
+
+    // when the central disconnects, print it out:
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
+  }
   // Blue Text Black Background
   myTOLED.windowClear();
   myTOLED.setWindowColorSet();
   
   myTOLED.setTextCursor(128, 0);
-  myTOLED.print(input);
+  if(switchCharacteristic.value() == 1){
+    myTOLED.print("BAM");
+  }
   delay(1000);
 }
