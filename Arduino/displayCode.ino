5ad5c8ed68fb8b4d9ce68870945d237cb185a28c
John Gerving <gervingjohn@gmail.com>
2020-04-04 21:39:16 -0700

    Update displayCode.ino

--- Arduino/displayCode.ino
+++ Arduino/displayCode.ino
@@ -1,6 +1,8 @@
 #include <HyperDisplay_UG2856KLBAG01.h>
 #include <HyperDisplay_SSD1309.h>
 #include <hyperdisplay.h>
+#include <ArduinoBLE.h>
+
 
 #define SERIAL_PORT Serial  
 #define WIRE_PORT Wire      // Used if USE_SPI == 0
@@ -22,11 +24,44 @@
   UG2856KLBAG01_I2C myTOLED;  // Declare a I2C-based Transparent OLED object called myTOLED
 #endif /* USE_SPI */
 
-char input[50] = "Hello world!";
+//char input[50] = "Hello world!";
+
+BLEService notifyMeService("19B10000-E8F2-537E-4F6C-D104768A1214"); // BLE LED Service
+
+// BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+BLEByteCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
 
 
 void setup() {
   Serial.begin(9600);
+
+  Serial1.begin(9600);
+  while (!Serial1);
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial1.println("starting BLE failed!");
+
+    while (1);
+  }
+
+  // set advertised local name and service UUID:
+  BLE.setLocalName("NotifyMe");
+  BLE.setAdvertisedService(notifyMeService);
+
+  // add the characteristic to the service
+  notifyMeService.addCharacteristic(switchCharacteristic);
+
+  // add service
+  BLE.addService(notifyMeService);
+
+  // set the initial value for the characeristic:
+  switchCharacteristic.writeValue(0);
+
+  // start advertising
+  BLE.advertise();
+
+  Serial1.println("NotifyMe Peripheral");
+  
 
 #if USE_SPI 
   SPI_PORT.begin();
@@ -43,7 +78,50 @@ void loop() {
   myTOLED.windowClear();
   myTOLED.setWindowColorSet();
   
-  myTOLED.setTextCursor(128, 0);
-  myTOLED.print(input);
-  delay(1000);
-}
+  // listen for BLE peripherals to connect:
+  BLEDevice central = BLE.central();
+  
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial1.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial1.println(central.address());
+
+    // while the central is still connected to peripheral:
+    while (central.connected()) {
+      // if the remote device wrote to the characteristic,
+      // use the value to control the LED:
+      /*
+      if(switchCharacteristic.value() == 1) {
+        myTOLED.setTextCursor(128, 0);
+        myTOLED.print("Text");
+      } else if(switchCharacteristic.value() == 2) {
+        myTOLED.setTextCursor(128, 0);
+        myTOLED.print("Missed Call");
+      } else if(switchCharacteristic.value() == 3) {
+        myTOLED.setTextCursor(128, 0);
+        myTOLED.print("Voicemail");
+      } else if(switchCharacteristic.value() == 4) {
+        myTOLED.setTextCursor(128, 0);
+        myTOLED.print("Gmail");
+      } else if(switchCharacteristic.value() == 5) {
+        myTOLED.setTextCursor(128, 0);
+        myTOLED.print("Discord");
+      } else if(switchCharacteristic.value() == 6) {
+        myTOLED.setTextCursor(128, 0);
+        myTOLED.print("Other");
+      }
+      */
+      if(switchCharacteristic.written()) {
+        myTOLED.print("test");
+      }
+    }
+
+    // when the central disconnects, print it out:
+    Serial1.print(F("Disconnected from central: "));
+    Serial1.println(central.address());
+  } else {
+    myTOLED.setTextCursor(128, 0);
+    myTOLED.print("No connection");
+  }
+ }
